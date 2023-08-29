e5e401529cf5b3bd294ba15987819bb01b233c0d
DhirajHolkar11 <143429479+DhirajHolkar11@users.noreply.github.com>
2023-08-29 12:31:45 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+function function1() {
+    let a=document.getElementById("color1");
+    a.style.visibility="visible";
+}
+
+
+function buttonfunction() {
+    let b=document.getElementById("color1");
+    b.style.visibility="hidden";
+}
